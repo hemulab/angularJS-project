f9ff9a1df9b0b777f27194bd97d3dd65d9e7fb2a
Wei Luo <Wei Luo>
2016-01-04 17:53:46 +0800

    up

--- app/Scripts/common.js
+++ app/Scripts/common.js
@@ -26,7 +26,6 @@ var indexApp = angular.module('index', []).
 		$scope.courses = courses;
 	});
 
-
 var systemApp = angular.module('system', ['ngRoute']).
 	controller('systemCtrl', ['$scope','$route','$routeParams','$location',function($scope, $route, $routeParams, $location){
 		$scope.$route = $route;
@@ -35,19 +34,22 @@ var systemApp = angular.module('system', ['ngRoute']).
      	$scope.hubs = hubs;
 		$scope.courses = courses;
 		$scope.listIsShown = false;
+		$scope.nowCourse = '高中语文';
+		$scope.sortBarPath = 'partials/user-hub-sortBar.html';
 		$scope.changeNowCourse = function(e) {
 			var e = e || window.event,
 				targ = e.target || e.srcElement;
 			$scope.nowCourse = targ.innerHTML;
 			$scope.listIsShown = false;
 		};
-		$scope.showCourseList = function() {
-			$scope.listIsShown = true;
+		$scope.toggleCourseList = function() {
+			$scope.listIsShown = !$scope.listIsShown;
 		};
 	}]).
 	config(function($routeProvider) {
 	  	$routeProvider.
-	  	when('/group-hub', {
+	  	when('/group-hub', {redirectTo:'/group-hub/manual'}).
+	  	when('/group-hub/:method', {
 		    templateUrl: 'partials/group-hub.html',
 		    controller: 'grouprHubCtrl'
 	  	}).
@@ -62,7 +64,10 @@ var systemApp = angular.module('system', ['ngRoute']).
 	  	otherwise({redirectTo:'/user-hub/saved'});
 	}).
 	controller('userRecordCtrl', ['$scope','$routeParams', function($scope, $routeParams){
+		cleanActive();
+		makeHubActiveAndChangeTitle($scope,'user');
 		var name = $routeParams.name, detail;
+		makeUserSideActive(name);
 		var set = [
 			{name:'saved', data:''},
 			{name:'downloaded', data:''},
@@ -76,8 +81,46 @@ var systemApp = angular.module('system', ['ngRoute']).
 			}
 		});
 		$scope.detail = detail;
-		$scope.hubContent = 'partials/user-hub-record.html';
-		if(name == 'question'||name == 'paper'){
-			$scope.hubContent = 'partials/user-hub-collection.html';
+		switch(name){
+			case 'downloaded':
+			$scope.hubContent = 'partials/user-hub-downloadedRecord.html';
+			break;
+			case 'question':
+			$scope.hubContent = 'partials/user-hub-questionCollection.html';
+			break;
+			case 'paper':
+			$scope.hubContent = 'partials/user-hub-paperCollection.html';
+			break;
+			default:
+			$scope.hubContent = 'partials/user-hub-savedRecord.html';
 		}
-	}]);
+	}]).
+	controller('grouprHubCtrl', ['$scope','$routeParams', function($scope, $routeParams){
+		cleanActive();
+		makeHubActiveAndChangeTitle($scope,'group');
+		var method = $routeParams.method, detail;
+		makeGroupChooseActive(method);
+	}])
+	;
+
+function cleanActive(){
+	var actived = document.getElementsByClassName('active');
+	for(var i = actived.length; i--;){
+		actived[i].className = actived[i].className.replace(' active', '');
+	}
+}
+function makeHubActiveAndChangeTitle ($s, hubId) {
+	var hub = document.getElementsByClassName(hubId)[0];
+	hub.className += ' active';
+	document.title = hub.innerHTML + ' | 万方在线组卷系统';
+}
+function makeUserSideActive(name){
+	var activedElem = document.getElementsByClassName('user-sideBar')[0].getElementsByClassName(name)[0],
+		activedElemParent = activedElem.parentNode;
+	activedElem.className += ' active';
+	activedElemParent.className += ' active';
+}
+function makeGroupChooseActive (method) {
+	var activedElem = document.getElementsByClassName(method)[0];
+	activedElem.className += ' active';
+}
